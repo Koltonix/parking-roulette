10c46d7809b15345ea59674289172eae55206830
Christopher Robertson <christopherprobertson@gmail.com>
2020-06-05 23:09:39 +0100

    - Created a simple class that rotates an object with a Lerp; however, due to eulerAngles and Quaternion differences it is causing it to Lerp in the incorrect  way due to the rotation.
    - To fix this I'm going to keep track of the rotation and manually change the values in a following commit.

---
+++ cross-roads-project/Assets/cross-roads/Scripts/Gameplay/General/ObjectRotator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Roads.Utilities
+{
+    public class ObjectRotator : MonoBehaviour
+    {
+        [SerializeField]
+        private float speed = 1.25f;
+        [SerializeField]
+        private float rotateInterval = 45.0f;
+        public Vector3 axis = Vector3.up;
+
+        private Coroutine rotating;
+
+        private void Start()
+        {
+            axis.x = (axis.x == 0) ? 0 : 1;
+            axis.y = (axis.y == 0) ? 0 : 1;
+            axis.z = (axis.z == 0) ? 0 : 1;
+        }
+
+        public void RotateObject(int direction)
+        {
+            //Sanity check ensuring it is either 1, 0, or -1
+            direction = (direction > 0) ? 1 : direction;
+            direction = (direction < 0) ? -1 : direction;
+
+            Vector3 rot = this.transform.rotation.eulerAngles;
+            Vector3 targetRotation = new Vector3 (axis.x * direction * (rot.x + rotateInterval),
+                                                  axis.y * direction * (rot.y + rotateInterval),
+                                                  axis.z * direction * (rot.y + rotateInterval));
+
+            if (rotating == null)
+                rotating = StartCoroutine(LerpRotation(targetRotation, speed));
+        }
+
+        private IEnumerator LerpRotation(Vector3 targetRotation, float speed)
+        {
+            Quaternion originalRotation = this.transform.rotation;
+
+            float t = 0.0f;
+            while (t <= 1.0f)
+            {
+                t += Time.deltaTime * speed;
+                this.transform.rotation = Quaternion.Lerp(originalRotation, Quaternion.Euler(targetRotation), t);
+
+                yield return new WaitForEndOfFrame();
+            }
+
+            rotating = null;
+        }
+    }
+}
